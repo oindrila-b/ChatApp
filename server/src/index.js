d31ce310dfd180b7481ed990c84a5d66aa98c35a
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-03-15 01:02:06 +0530

    Adding logic, data, layout

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,29 @@
-import path from 'path'
-import { fileURLToPath } from 'url';
-import {Server} from 'socket.io'
+
+import { Server } from 'socket.io'
 import express from 'express'
 
 const PORT = 3000;
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-
 const app = express();
- 
 
+const ADMIN = "Admin"
 
 const expressServer = app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 });
 
+// User State
+const UsersState = {
+    users: [],
+    setUsers: function (newUsersArray) {
+        this.users = newUsersArray
+    }
+}
+
 const io = new Server(expressServer, {
-    cors:{
+    cors: {
         origin: process.env.NODE_ENV === 'production' ? 'false' :
-         ['http://localhost:5500', 'http://127.0.0.1:5500']
+            ['http://localhost:5500', 'http://127.0.0.1:5500']
     }
 })
 
@@ -27,23 +31,127 @@ io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
     // send message to connected user
-    socket.emit('message', "Welcome to Chat App")
+    socket.emit('message', buildMessage(ADMIN, "Welcome to Chat App!"))
 
-     // send message to all the user
-     socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`)
 
-     // listening for message event
-    socket.on('message', data => {
-        console.log(data)
-        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    socket.on('enterRoom', ({ name, room }) => {
+
+        const prevRoom = getUser(socket.id)?.room
+
+        if (prevRoom) {
+            socket.leave(prevRoom)
+            io.to(prevRoom).emit('message', buildMessage(ADMIN, `${name} has left the room`))
+        }
+
+        const user = activateUser(socket.id, name, room);
+
+        if (prevRoom) {
+            io.to(prevRoom).emit('activeUsers', {
+                users: getUsersInRoom(prevRoom)
+            })
+        }
+
+        socket.join(user.room)
+
+        //To user who joined
+        socket.emit('message', buildMessage(ADMIN, `You have joined the ${user.room} chat room`))
+
+        // To the rest of the users in the room
+        socket.broadcast.to(user.room).emit('message', buildMessage(ADMIN, `${user.name} has joined the ${user.room} chat room`))
+
+
+        // Update user list
+        io.to(user.room).emit('activeUsers', {
+            users: getUsersInRoom(prevRoom)
+        })
+
+        // update rooms list for all
+        io.emit('activeRooms', {
+            rooms: getAllActiveRooms()
+        })
+       
     })
+
 
     // on user disconnection
     socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`)
+        const user = getUser(socket.id)
+        userDisconnects(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('message', buildMessage(ADMIN, `${user.name} has left the room`))
+            io.to(user.room).emit('activeUsers', {
+                users: getUsersInRoom(user.room)
+            })
+
+            io.emit('activeRooms', {
+                rooms: getAllActiveRooms()
+            })
+
+            console.log(`User ${socket.id} disconnected`)
+        }
+    })
+
+
+
+    // listening for message event
+    socket.on('message', ({name, text}) => {
+        const room  =  getUser(socket.id)?.room
+        if(room) {
+            io.to(room).emit('message', buildMessage(name, text))
+        }
+        
     })
 
     socket.on('activity', (name) => {
-        socket.broadcast.emit('activity', name)
+        const room  =  getUser(socket.id)?.room
+        if(room) {
+            socket.broadcast.to(room).emit('activity', name)
+        }
     })
 })
+
+
+function buildMessage(name, text) {
+    return {
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }).format(new Date())
+    }
+}
+
+// User state util functions
+function activateUser(id, name, room) {
+    const user = { id, name, room }
+    UsersState.setUsers([
+        ...UsersState.users.filter(user => user.id !== id),
+        user
+    ])
+    return user
+}
+
+function userDisconnects(id) {
+    UsersState.setUsers(
+        UsersState.users.filter(user => user.id != id)
+    )
+}
+
+function getUser(id) {
+    return UsersState.users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room) {
+    console.log(UsersState.users)
+    console.log(Array.isArray(UsersState.users))
+    return UsersState.users.filter((user) => user.room === room)
+}
+
+
+function getAllActiveRooms() {
+    return Array.from(new Set(UsersState.users.map(user => user.room)))
+}
+
