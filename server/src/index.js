97e62aaa47b2cfa84b873915019c1a25891264a4
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-03-14 13:28:59 +0530

    MOD : Put everything in server side

--- server/src/index.js
+++ server/src/index.js
@@ -5,11 +5,11 @@ import express from 'express'
 
 const PORT = 3000;
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')))   
+ 
 
 
 const expressServer = app.listen(PORT, () => {
